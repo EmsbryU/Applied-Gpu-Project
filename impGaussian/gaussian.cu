db0d8ed843050c41de24bd3aad63159e6da8271e
Anton <antonulin00@gmail.com>
2024-12-18 14:46:26 +0100

    more readable array indexing

--- impGaussian/gaussian.cu
+++ impGaussian/gaussian.cu
@@ -290,8 +290,13 @@ __global__ void Fan1(float *m_cuda, float *a_cuda, int Size, int t)
 {   
 	//if(threadIdx.x + blockIdx.x * blockDim.x >= Size-1-t) printf(".");
 	//printf("blockIDx.x:%d,threadIdx.x:%d,Size:%d,t:%d,Size-1-t:%d\n",blockIdx.x,threadIdx.x,Size,t,Size-1-t);
-	if(threadIdx.x + blockIdx.x * blockDim.x >= Size-1-t) return;
-	*(m_cuda+Size*(blockDim.x*blockIdx.x+threadIdx.x+t+1)+t) = *(a_cuda+Size*(blockDim.x*blockIdx.x+threadIdx.x+t+1)+t) / *(a_cuda+Size*t+t);
+	unsigned int gthid = threadIdx.x + blockIdx.x * blockDim.x;
+
+	if(gthid >= Size-1-t) return;
+	m_cuda[Size * (gthid + t + 1) + t] = a_cuda[Size*(gthid + t + 1) + t] / a_cuda[Size * t + t];
+
+	// if(threadIdx.x + blockIdx.x * blockDim.x >= Size-1-t) return;
+	// *(m_cuda+Size*(blockDim.x*blockIdx.x+threadIdx.x+t+1)+t) = *(a_cuda+Size*(blockDim.x*blockIdx.x+threadIdx.x+t+1)+t) / *(a_cuda+Size*t+t);
 }
 
 /*-------------------------------------------------------
