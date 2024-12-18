8b1a15892a983ac0bbb0e60248729506a126512e
Anton <antonulin00@gmail.com>
2024-12-18 11:37:47 +0100

    added some prints to compileandcompare (mostly test commit)

--- compileAndCompare.sh
+++ compileAndCompare.sh
@@ -5,5 +5,7 @@ cd ../../
 cd impGaussian/
 make
 cd ../
+echo "Original:"
 cuda/gaussian/gaussian -f data/gaussian/matrix16.txt -q
+echo "Improved:"
 impGaussian/gaussian -f data/gaussian/matrix16.txt -q
